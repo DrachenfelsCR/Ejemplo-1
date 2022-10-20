fd2000d406908963d1ce9d4549d8b5967e2c04b1
DrachenfelsCR <davidquesadaromero@gmail.com>
2022-10-19 22:53:53 -0600

    components custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements : any = [{type: 'server', name: 'Test-server-1', content: 'Just a test!'},{type: 'server', name: 'Test-server-2', content: 'Just a test!'}];
+
+  onServerAdded(serverData : {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData : {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
